f83745432b83f8b641290ea1cf7166f842af68b0
logvinov Mikhail <mlogvinov2016@gmail.com>
2020-08-25 15:44:48 +0300

    completed homework

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,30 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+
+	let averageScore = (physics + geography + algebra) / 3;
+
+	return averageScore;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	let myName = userName;
+	let message = (`Привет, мир! Меня зовут ${myName}`);
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	
+	let result = (x * y + 5 * z + x -1);
+
+    return result;
 }
