8e84fe9f0203dd2bbf0ff0e781f190117168ad31
Logvinov Mikhail <mlogvinov2016@gmail.com>
2020-09-01 22:10:51 +0300

    homework_1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,46 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict"; 
+    
+    let d = b**2 -4*a*c;
+
+    let x = [];
+
+    if (d >= 0) {
+        x[0] = (-b + Math.sqrt(d)) / 2 * a;
+    }
+
+    if (d > 0) {
+        x[1] = (-b - Math.sqrt(d)) / 2 * a;
+    }
+
+     return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    
+    let sumOfMark =0;
+    let len = marks.length;
+
+    if (len == 0) {
+        return 0;
+    } else if (len > 5) {
+        return "Количество оценок больше 5. Учитываются первые 5"
+    }
+    
+    marks.splice(5);
+
+    for (let i = 0; i < len; i++){
+       sumOfMark += marks[i];
+       let averageMark =  sumOfMark / len;
+
+       return averageMark;
+    }
+
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    
+    
+    
+    return result;
 }
