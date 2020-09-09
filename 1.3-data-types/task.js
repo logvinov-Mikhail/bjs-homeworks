daba76ab686e2d5558bbc670c22e1f409506c5e8
logvinov Mikhail <mlogvinov2016@gmail.com>
2020-09-09 15:21:28 +0300

    dz-1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,29 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict"; 
+   
+    //console.log(typeof percent);
+    //console.log(typeof contribution);
+    //console.log(typeof amount);
+    
+    let months = ((date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()));
+    //console.log(typeof months);
+    let percentMonth = (parseInt(percent)/12)/100;
+    //console.log(typeof percentMonth);
+    let accruedMonths = (amount - contribution)*(percentMonth + percentMonth/(((1 + percentMonth)**months)-1));
+
+    let totalAmount = Number((accruedMonths * months).toFixed(2));
+    //console.log(typeof totalAmount);
+    return totalAmount;
+
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    if (name == "" || name == null || name == undefined) {
+        greeting = 'Привет, мир! Меня зовут Аноним','.';
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}`,`.`;
+    }
+
+     return greeting;
 }
