5fa448eee4369df8ce72206a133a8e0195a685d8
logvinov Mikhail <mlogvinov2016@gmail.com>
2020-11-18 15:42:09 +0300

    dz-3.1

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,44 +1,67 @@
 // Задача №1 
 
 function parseCount(result) {
-    
     if (isNaN(Number.parseInt(result))) {
         const numberError = new Error("Невалидное значение");
-        throw numberError;
+        throw numberError;  
     } else {
         return Number.parseInt(result);
-    }  
+    }
 }
 
 function validateCount(count) {
     try {
         parseCount(count);  
     } catch(e) {
-        return console.log("ошибку перехватил");  
+        const numberError = new Error("Невалидное значение");
+        throw numberError; 
     }   
-    return console.log(parseCount(count));    
+    return parseCount(count);   
 }
 
 // Задача №2 
 
 class Triangle {
     constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
         try {
             if ((a + b) < c || (a + c) < b || (b + c) < a) {
-                const trError = new Error("Невалидное значение");
+                
+                const trError = new Error("Треугольник с такими сторонами не существует");
                 throw trError;
             } 
         } catch(e) {
-            return console.log("ошибку перехватил");  
+            console.log("ошибку перехватил");  
         } 
-        return console.log("все хорошо");
+    }
+    
+    getPerimeter() {
+        return (this.a + this.b + this.c);
+    }
+
+    getArea() {
+        const p = (this.a + this.b + this.c)*0.5; //Вычисление полупериметра
+        const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)); //Вычисление площади
+        return s.toFixed(3);
     }
 }
 
-const test = new Triangle(1, 2, 1)
+const test = new Triangle(3, 2, 4);
+console.log(test);
+console.log(test.getPerimeter());
+console.log(test.getArea());
 
 function getTriangle(a, b, c) {
+    
+    // try {
 
+    // } catch(e) {
+    //     const trError = new Error("Треугольник с такими сторонами не существует");
+    //     throw trError; 
+    // }   
+    return new Triangle(a, b, c); 
 }
 
-//Добрый день. Если кто может помочь с решением задачи 3.1 еще из курса Основы JavaScript напишите в лс. Буду признателен!
